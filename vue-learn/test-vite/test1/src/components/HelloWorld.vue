65e974ae31d58b5fc46060e04cf570642afcac80
xieqi <xieqi@yy.com>
2020-07-07 16:32:43 +0800

    feat(vue-leart): vue learn

---
+++ vue-learn/test-vite/test1/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code>a to test hot module replacement.</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
