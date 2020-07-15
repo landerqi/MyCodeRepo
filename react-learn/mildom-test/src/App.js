8d2091492270be48bfc55982697b2c0aa1c84dbb
xieqi <xieqi@yy.com>
2020-07-15 17:25:13 +0800

    feat: add test code

--- react-learn/mildom-test/src/App.js
+++ react-learn/mildom-test/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Header name="header-component"/>
+          <Header name="Todo List"/>
       </header>
     </div>
   );
--- react-learn/mildom-test/src/view/header/header.jsx
+++ react-learn/mildom-test/src/view/header/header.jsx
@@ -4,10 +4,6 @@ import PropTypes from "prop-types"
 import SubHeader from "../../components/sub_header/index.tsx"
 
 class Header extends Component {
-    constructor (props) {
-        super(props)
-    }
-
     render() {
         return (
             <div className="app-header">
